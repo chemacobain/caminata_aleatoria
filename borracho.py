1cef6b9a688c69e4a204eee49eb89f561e156d4b
Chema Cobain <chemacubain@gmail.com>
2021-02-26 11:55:51 -0600

    first commit

---
+++ borracho.py
@@ -0,0 +1,18 @@
+import random
+
+class Borracho:
+
+    def __init__(self, nombre):
+        self.name = nombre
+
+class  borrachoTradicional(Borracho):
+
+    def __init__(self, nombre):
+        super().__init__(nombre)
+
+    def camina(self):
+        sig_paso = [(0,1), (0,-1), (1,0),(-1,0)]
+        d = random.choices(range(len(sig_paso)),weights=(40,20,20,10),k=1)
+        dint = int(d[0])
+        return sig_paso[dint]
+        
